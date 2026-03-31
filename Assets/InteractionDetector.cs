9d058b6b520b7f3f4074698d143c4d3935fb02a9
phil5160 <gdq5160@autuni.ac.nz>
2026-03-31 17:06:25 +1300

    Added interaction script
    havent added dialogue script yet

---
+++ Assets/InteractionDetector.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InteractionDetector : MonoBehaviour
+{
+    private IInteractable interactableInRange = null; //Closest Interactable 
+    public GameObject interactionIcon;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        interactionIcon.SetActive(false);
+    }
+
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            interactableInRange?.Interact();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.TryGetComponent(out IInteractable interactable) && interactable.CanInteract())
+        {
+            interactableInRange = interactable;
+            interactionIcon.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out IInteractable interactable) && interactable == interactableInRange)
+        {
+            interactableInRange = null;
+            interactionIcon.SetActive(false);
+        }
+    }
+
+}
