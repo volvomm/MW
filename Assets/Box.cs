c9063510afcd6101f4a49cfd571acd3841501558
phil5160 <gdq5160@autuni.ac.nz>
2026-04-01 16:22:58 +1300

    npc script made
    made the npc script for the box

---
+++ Assets/Box.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Box : MonoBehaviour, IInteractable 
+{
+    public BoxDialogue dialogueData;
+    public GameObject dialoguePanel;
+    public TMP_Text dialougeText, nameText;
+    public Image portraitImage;
+
+    private int dialogueIndex;
+    private bool isTyping, isDialogueActive;
+
+    public bool CanInteract()
+    {
+        return !isDialogueActive;
+    }
+
+    public void Interact()
+    {
+        if (dialogueData == null || (!isDialogueActive))
+            return;
+
+        if(isDialogueActive)
+        {
+            NextLine();
+        }
+        else
+        {
+            StartDialogue();
+        }
+    }
+    void StartDialogue()
+    {
+        isDialogueActive = true;
+        dialogueIndex = 0;
+
+        nameText.SetText(dialogueData.npcName);
+        portraitImage.sprite = dialogueData.npcPortrait;
+
+        dialoguePanel.SetActive(true);
+
+        StartCoroutine(TypeLine());
+        
+    }
+
+    void NextLine()
+    {
+        if (isTyping)
+        {
+            StopAllCoroutines();
+            dialougeText.SetText(dialogueData.dialogueLines[dialogueIndex]);
+            isTyping = false;
+        }
+        else if(++dialogueIndex < dialogueData.dialogueLines.Length)
+        {
+            //If another line, type next line
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            EndDialogue();
+        }
+    }
+
+    IEnumerator TypeLine()
+    {
+        isTyping = true;
+        dialougeText.SetText("");
+
+        foreach(char letter in dialogueData.dialogueLines[dialogueIndex])
+        {
+            dialougeText.text += letter;
+            yield return new WaitForSeconds(dialogueData.typingSpeed);
+        }
+
+        isTyping = false;
+
+        if(dialogueData.autoProgressLines.Length > dialogueIndex && dialogueData.autoProgressLines[dialogueIndex])
+        {
+            yield return new WaitForSeconds(dialogueData.autoProgressDelay);
+            NextLine();
+        }
+    }
+    public void EndDialogue()
+    {
+        StopAllCoroutines();
+        isDialogueActive = false;
+        dialougeText.SetText("");
+        dialoguePanel.SetActive(false);
+    }
+}
